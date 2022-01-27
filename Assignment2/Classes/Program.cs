6a72da3b904971f47d2b9686fe1d0d85d12490ab
VsrnVarma <98328610+VsrnVarma@users.noreply.github.com>
2022-01-27 16:38:30 +0530

    Update Program.cs

--- Assignment2/Classes/Program.cs
+++ Assignment2/Classes/Program.cs
@@ -1,4 +1,14 @@
-﻿using System;
+﻿Create a class called Accounts which has data members like Account no, Customer name, Account type, Transaction type (d/w), amount, balance
+    D->Deposit
+    W->Withdrawal
+    -write a function that updates the balance depending upon the transaction type
+       -If transaction type is deposit call the credit(int amount) function and update balance
+	   -If transaction type is withdraw call debit(int amt)function and update balance
+    -Pass the other information like Acount no,name,acc type through constructor
+    -call the show data method to display the values.
+
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
