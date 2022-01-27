2f16abaea52e0682805517b05efc133f201014f4
VsrnVarma <98328610+VsrnVarma@users.noreply.github.com>
2022-01-27 16:39:11 +0530

    Update AbsStudent.cs

--- Assignment2/Classes/AbsStudent.cs
+++ Assignment2/Classes/AbsStudent.cs
@@ -1,4 +1,11 @@
-﻿using System;
+﻿Create an Abstract class Student with  Name, StudentId, Grade as members 
+and also an abstract method Boolean Ispassed(grade) which takes grade as an input and checks whether student passed the course or not.  
+Create 2 Sub classes Undergraduate and Graduate that inherits all members of the student and overrides Ispassed() method 
+For the UnderGrad class, if the grade is above 70.0, then isPassed returns true, otherwise it returns false. For the Grad class, 
+if the grade is above 80.0, then isPassed returns true, otherwise false.
+
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
