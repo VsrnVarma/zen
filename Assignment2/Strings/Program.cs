a8559e657589b8e508984f74757301118cfd1b8a
VsrnVarma <98328610+VsrnVarma@users.noreply.github.com>
2022-01-27 16:39:56 +0530

    Update Program.cs

--- Assignment2/Strings/Program.cs
+++ Assignment2/Strings/Program.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿1.	Write a program in C# to accept a word from the user and display the length of it.
+2.	Write a program in C# to accept a word from the user and display the reverse of it. 
+3.	Write a program in C# to accept two words from user and find out if they are same.
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
