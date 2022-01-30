4d5bbfe2c443352a81ec882ce50090002dd814fe
VsrnVarma <98328610+VsrnVarma@users.noreply.github.com>
2022-01-30 20:16:57 +0530

    Add files via upload

---
+++ Assignment3/ExceptionHandling/Loan.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionHandling
+{
+    class LoanException : ApplicationException
+    {
+        public LoanException(string msg) : base(msg)
+        {
+        }
+    }
+
+    class LoanProcess
+    {
+        int Lno;
+        string CustName;
+        public double LoanAmount;
+        double EmiAmount,Balance;
+
+        public LoanProcess(int lno,string name,double bal)
+        {
+            Lno = lno;
+            CustName = name;
+            Balance = bal;
+        }
+
+        public void CaluculateEMI()
+        {
+            int rate = 13,n=36;
+            double r = ((double)rate / (double)n )/100;
+
+            EmiAmount = LoanAmount * r * (Math.Pow((1 + r), n) / (Math.Pow((1 + r), n) - 1));
+
+        }
+
+        public void CheckBalance()
+        {
+            if (Balance < EmiAmount)
+            {
+                throw (new LoanException("Not Sufficient Balance to repay Loan"));
+            }
+            else
+            {
+                Console.WriteLine("Loan Repayed");
+            }
+        }
+
+    }
+
+    class Loan
+    {
+        static void Main()
+        {
+            LoanProcess loan = new LoanProcess(1234,"Akhil",10000.0);
+            Console.WriteLine("Enter the Loan amount: ");
+            loan.LoanAmount = Convert.ToDouble(Console.ReadLine());
+
+            loan.CaluculateEMI();
+            try
+            {
+                loan.CheckBalance();
+            }
+            catch(LoanException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
