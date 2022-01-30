929bb2dd068c32d3fec1e88fe5e03715ac78ca2c
VsrnVarma <98328610+VsrnVarma@users.noreply.github.com>
2022-01-30 20:17:57 +0530

    Assignment 3

--- Assignment3/ExceptionHandling/Loan.cs
+++ Assignment3/ExceptionHandling/Loan.cs
@@ -1,4 +1,7 @@
-﻿using System;
+Write a class called LoanProcess with Loan_No, Customer Name, LoanAmount, EMI_Amount, Account_Balance as its members. Create a method calculate_EMI() for the LoanAmount , with the rate of interest as 13% for a total of 3 years and store it in the EMI_Amount. 
+The rest of the information to be passed through constructors. Write another function CheckBalance() which checks if the Account_Balance is less than the EMI_AMount. If yes then throw a custom exception. Display " Not Sufficient Balance to repay Loan" in the finally. Give explanatory comments.
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
