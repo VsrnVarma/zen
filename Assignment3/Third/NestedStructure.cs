c89a82a70c8dcf41b033c68209da0086a113737c
VsrnVarma <98328610+VsrnVarma@users.noreply.github.com>
2022-01-30 20:24:26 +0530

    Add files via upload

---
+++ Assignment3/Third/NestedStructure.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Third
+{
+    public struct Dob
+    {
+        public int Day;
+        public int Month;
+        public int Year;
+    }
+
+    struct Employee
+    {
+        public string Name;
+        public Dob Date;
+    }
+
+    class NestedStructure
+    {
+        static void Main()
+        {
+            
+            Employee[] emp = new Employee[2];
+
+            for (int i = 0; i < 2 ; i++)
+            {
+                Console.Write($"Enter The name of the employee{i+1}: ");
+                string name = Console.ReadLine();
+                emp[i].Name = name;
+                Console.WriteLine();
+
+                Console.WriteLine("Enter the Dob---");
+                Console.Write("The day of birth: ");
+                int day = Convert.ToInt32(Console.ReadLine());
+                emp[i].Date.Day = day;
+
+                Console.Write("The month of birth: ");
+                int month = Convert.ToInt32(Console.ReadLine());
+                emp[i].Date.Month = month;
+
+                Console.Write("The year of birth: ");
+                int year = Convert.ToInt32(Console.ReadLine());
+                emp[i].Date.Year = year;
+                Console.WriteLine();
+            }
+
+            /*for(int i = 0; i < 2; i++)
+            {
+                Console.WriteLine($"Name of the employee{i+1}: {emp[i].Name}");
+                Console.WriteLine($"Date of birth: {emp[i].Date.Day}/{emp[i].Date.Month}/{emp[i].Date.Year}");
+                Console.WriteLine();
+            }*/
+        }
+    }
+}
