655542320eabd93bd67533e613c2201f4173ed72
VsrnVarma <98328610+VsrnVarma@users.noreply.github.com>
2022-01-30 20:22:30 +0530

    Assignment 3

--- Assignment3/Second/InterfaceEg.cs
+++ Assignment3/Second/InterfaceEg.cs
@@ -1,4 +1,7 @@
-﻿using System;
+Create an Interface IStudent with StudentId and Name as Properties, ShowDetails() as its method. 
+Create 2 classes Dayscholar and Resident that implements the interface Properties and Methods. Test The functionalities.
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
