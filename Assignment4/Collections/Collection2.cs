27e32b00e64dbdd5bddad1f121373c0a54c33850
VsrnVarma <98328610+VsrnVarma@users.noreply.github.com>
2022-02-03 18:58:36 +0530

    Assignment 4

---
+++ Assignment4/Collections/Collection2.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Collections
+{
+    class Products
+    {
+        int Pid;
+        string PName;
+        public double Price;
+
+        public Products(int id, string name, double price)
+        {
+            Pid = id;
+            PName = name;
+            Price = price;
+        }
+
+        public override string ToString()
+        {
+            return $"Product Id: {Pid} Name: {PName} costs: {Price}";
+        }
+    }
+
+    class Collection2
+    {
+        static void Main()
+        {
+            List<Products> plist = new List<Products>();
+            plist.Add(new Products(24, "Dell Laptop", 49999.00));
+            plist.Add(new Products(05, "Mi TV", 34999.00));
+            plist.Add(new Products(36, "Washing Machine", 45999.00));
+            plist.Add(new Products(01, "Cell Phone", 16999.00));
+            plist.Add(new Products(25, "Hp Laptop", 42999.00));
+            plist.Add(new Products(06, "TCL TV", 29999.00));
+            plist.Add(new Products(15, "Mixer", 14999.00));
+            plist.Add(new Products(16, "Grinder", 19499.00));
+            plist.Add(new Products(14, "Home Theatre", 64999.00));
+            plist.Add(new Products(19, "Sound System", 24450.00));
+
+            var res = from n in plist
+                      orderby n.Price
+                      select n.ToString();
+
+            Console.WriteLine("List after Sorting------");
+
+            Console.WriteLine(String.Join(Environment.NewLine, res));
+        }
+    }
+}
---
+++ Assignment4/Collections/Collection3.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace Collections
+{
+    class Student
+    {
+        int Sid;
+        string SName;
+        double Marks;
+
+        public Student(int id, string name, double marks)
+        {
+            Sid = id;
+            SName = name;
+            Marks = marks;
+        }
+
+        public override string ToString()
+        {
+            return $"Student Id: {Sid} Name: {SName} scored: {Marks}";
+        }
+    }
+
+    class Collection3
+    {
+        static void Main(string[] args)
+        {
+            List<Student> stu = new List<Student>();
+            stu.Add(new Student(10, "Ashraf", 66.5));
+            stu.Add(new Student(05, "Sreeram", 78.0));
+            stu.Add(new Student(16, "Rahul", 70.0));
+
+            foreach (Student s in stu)
+            {
+                Console.WriteLine(s.ToString());
+            }
+
+            Console.WriteLine("-------Array List-------");
+            ArrayList alist = new ArrayList();
+            alist.Add(24);
+            alist.Add("Good");
+            alist.Add(456.89);
+            alist.Add(30.4f);
+            foreach(var item in alist)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine("---------Hash Table-------");
+            Hashtable ht = new Hashtable();
+            ht.Add("S01", "Varma");
+            ht.Add("S02","Yogesh");
+            foreach(DictionaryEntry d in ht)
+            {
+                Console.WriteLine(d.Key+" "+d.Value);
+            }
+
+            Console.WriteLine("--------Dictionary--------");
+            Dictionary<int, string> dict = new Dictionary<int, string>();
+            dict.Add(20, "Sreeram");
+            dict.Add(19, "Dheeraj");
+            foreach (KeyValuePair<int,string> d in dict)
+            {
+                Console.WriteLine(d.Key + " " + d.Value);
+            }
+
+            Console.WriteLine("--------Sorted List--------");
+            SortedList slist = new SortedList();
+            slist.Add(26, "Sreeram");
+            slist.Add(29, "Dheeraj");
+            slist.Add(10, "Moses");
+            foreach (var item in slist.Values)
+            {
+                Console.WriteLine(item);
+            }
+
+        }
+    }
+}
