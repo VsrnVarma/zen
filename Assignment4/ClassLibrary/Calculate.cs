e052a16b8517c1161688123357cd24ec4fb8dc2b
VsrnVarma <98328610+VsrnVarma@users.noreply.github.com>
2022-02-03 20:56:00 +0530

    Assignment 4

---
+++ Assignment4/ClassLibrary/Calculate.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CLibrary
+{
+    public class Calculate
+    {
+        int Age;
+        int TotalFare = 500;
+
+        public void CalculateConcession()
+        {
+            Console.Write("Enter age: ");
+
+            Age = Convert.ToInt32(Console.ReadLine());
+
+            if (Age <= 5)
+            {
+                Console.WriteLine("Little Champs---Free Ticket");
+            }
+            else if (Age > 60)
+            {
+                double concession = 0.3 * TotalFare;
+                double cfare = TotalFare - concession;
+                Console.WriteLine("Senior Citizen "+cfare);
+            }
+            else
+            {
+                Console.WriteLine("Ticket booked "+TotalFare);
+            }
+        } 
+    }
+}
