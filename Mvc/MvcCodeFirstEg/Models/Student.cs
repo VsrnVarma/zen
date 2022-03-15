df20128b838b89470d27ea85e74fe7951601f613
VsrnVarma <98328610+VsrnVarma@users.noreply.github.com>
2022-03-15 22:33:36 +0530

    cf

---
+++ Mvc/MvcCodeFirstEg/Models/Student.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MvcCodeFirstEg.Models
+{
+    [Table("TblStudent")]
+    public class Student
+    {
+        [Key]
+        public int StudentId { get; set; }
+        public string Name { get; set; }
+        public string City { get; set; }
+        public string Branch { get; set; }
+        public int Percentage { get; set; }
+    }
+}
