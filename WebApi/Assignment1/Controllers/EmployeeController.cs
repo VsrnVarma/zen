14a036d5b8fec4ba19d005cf78f5b1e7e881d5bc
VsrnVarma <98328610+VsrnVarma@users.noreply.github.com>
2022-03-20 15:36:42 +0530

    Ass1

---
+++ WebApi/Assignment1/Controllers/EmployeeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Assignment1.Models;
+
+namespace Assignment1.Controllers
+{
+    public class EmployeeController : ApiController
+    {
+        public IHttpActionResult GetEmp(int id)
+        {
+            ZensarDBEntities ze = new ZensarDBEntities();
+            var result = ze.GetSalaryById(id).ToList();
+            return Ok(result);
+        }
+    }
+}
